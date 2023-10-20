11cdf264cbd15c3d6976ae52942f5443647411a7
Hakan Karakuş <hakan.karakus@boun.edu.tr>
2023-10-20 17:27:02 +0300

    functions names without trailing x, simpler function calls, remove some repetitive prints

--- app/page.tsx
+++ app/page.tsx
@@ -329,9 +329,6 @@ export default function Home() {
     console.log('Args', args)
     let newExtData: ExtData = { ...extData }
     newExtData.extAmount = BigNumber.from(extData.extAmount).toBigInt()
-    // extData.extAmount = BigNumber.from(extData.extAmount).toBigInt()
-    // await genpp()
-    // await prove()
     console.log('INPUT.JSON->')
     console.log(JSON.stringify(membershipProof))
     if (membershipProof) {
@@ -339,8 +336,8 @@ export default function Home() {
       const startjson = JSON.stringify({ step_in: [membershipProof[0].step_in] })
       console.log('inputjson', inputjson)
       console.log('startjson', startjson)
-      await genpp()
-      const proof = await prove(inputjson, startjson)
+      await workerProvider.generate_public_parameters()
+      const proof = await workerProvider.prove_membership(inputjson, startjson)
       console.log(proof)
     }
     await transact({ args, extData: newExtData })
@@ -373,27 +370,6 @@ export default function Home() {
     }
     const hash = await walletClient.writeContract(request)
     console.log(hash)
-  }
-
-  async function genpp() {
-    // workerProvider.workerSetup(ChainId.XDAI)
-    console.log('genpp called')
-    let ppx = await workerProvider.generate_ppx()
-    console.log('genpp done', ppx)
-  }
-
-  async function prove(inputjson: string, startjson: string) {
-    // workerProvider.workerSetup(ChainId.XDAI)
-    console.log('prove called')
-    await workerProvider.provex(inputjson, startjson)
-    console.log('prove done')
-  }
-
-  async function verify() {
-    // workerProvider.workerSetup(ChainId.XDAI)
-    console.log('verify called')
-    await workerProvider.verifyx()
-    console.log('verify done')
   }
 
   return (
