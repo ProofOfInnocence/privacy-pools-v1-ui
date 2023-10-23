6cb251960a5832e800bc586f12204f4151c491f4
Ekrem BAL <mail.ekrembal@gmail.com>
2023-10-23 12:50:41 +0300

    fix get cid

--- utilities/getIPFSCid.ts
+++ utilities/getIPFSCid.ts
@@ -3,6 +3,8 @@ import { ethers } from 'ethers'
 type MembershipProof = string
 
 export function getIPFSCid(file: MembershipProof) {
-  const cid = ethers.utils.keccak256(file)
+  console.log('getIPFSCid is called', file)
+  const cid = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(file))
+  console.log('cid:', cid)
   return cid
 }
