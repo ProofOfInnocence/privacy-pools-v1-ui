e490d69f41a54c16a92d898fc5e27e1ffecb6241
benbaho <aydinbahadirtuna@gmail.com>
2023-12-28 16:43:17 +0300

    unused react imports removed

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 type Operation = {
   ButtonName: string
   Function: () => void
--- components/WrapEtherComponent.tsx
+++ components/WrapEtherComponent.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useBalance } from 'wagmi'
 
 interface WrapEtherProps {
