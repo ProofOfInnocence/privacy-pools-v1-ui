68eb5eea932bd375867cc585b6c2424306d8ca67
Ekrem BAL <mail.ekrembal@gmail.com>
2024-02-27 12:43:04 +0300

    Fix new input generation poi

--- assets/nova.worker.js
+++ assets/nova.worker.js
@@ -10,12 +10,13 @@ async function generate_params({ mode, pp_path, base }, [port]) {
     console.log('mode: ', mode, ', pppath: ', pp_path, 'base: ', base)
     const multiThread = await import('nova_scotia_browser')
     await multiThread.default()
+    console.log('Calling gen params')
     //await multiThread.initThreadPool(navigator.hardwareConcurrency);
     let pp = await multiThread.generate_params(mode, pp_path, base)
     self.$pp = pp
-    port.postMessage({ result: pp })
-  } catch (e) {
-    throw e
+    // port.postMessage({ result: pp })
+  } catch (err) {
+    port.postMessage({ errorMessage: err.message })
   }
 }
 
@@ -25,32 +26,29 @@ async function generate_proof({ r1cs_path, wasm_path, mode, input_path_or_str, s
     await multiThread.default()
     console.log(r1cs_path, wasm_path, mode, input_path_or_str, start_path_or_str, base)
     //await multiThread.initThreadPool(navigator.hardwareConcurrency);
+    // multiThread.init_panic_hook()
     let proof = await multiThread.generate_proof(self.$pp, r1cs_path, wasm_path, mode, input_path_or_str, start_path_or_str, base)
     self.$proof = proof
     port.postMessage({ result: proof })
-  } catch (e) {
-    throw e
+  } catch (err) {
+    port.postMessage({ errorMessage: err.message })
   }
 }
 
 const listener = ({ data, ports }) => {
-  try {
-    self.postMessage(data)
-    switch (data.eventName) {
-      case workerEvents.INIT_WORKER:
-        initWorker(5)
-        break
-      case workerEvents.GENERATE_PP:
-        generate_params(data.payload, ports)
-        console.log('public parameters generated')
-        break
-      case workerEvents.PROVE:
-        generate_proof(data.payload, ports)
-        console.log('prove completed')
-        break
-    }
-  } catch (e) {
-    throw e
+  self.postMessage(data)
+  switch (data.eventName) {
+    case workerEvents.INIT_WORKER:
+      initWorker(5)
+      break
+    case workerEvents.GENERATE_PP:
+      generate_params(data.payload, ports)
+      console.log('public parameters generated')
+      break
+    case workerEvents.PROVE:
+      generate_proof(data.payload, ports)
+      console.log('prove completed')
+      break
   }
 }
 
